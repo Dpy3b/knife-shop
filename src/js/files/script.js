94783a0c484e9fd82af3516af690dcc3e0dac1c5
Dpy3b <57526052+Dpy3b@users.noreply.github.com>
2021-12-15 11:21:04 +0300

    some commit

---
+++ src/js/files/script.js
@@ -0,0 +1,20 @@
+// Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
+// import { isMobile } from "./functions.js";
+
+document.addEventListener('click', documentActions);
+function documentActions(e) {
+    const targetElement = e.target;
+    if (targetElement.closest('[data-parent]')){
+        const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
+        const subMenu = document.querySelectorAll(`[data-submenu="${subMenuId}"]`);
+        const catalogMenu = document.querySelectorAll('.catalog-header');
+        if(subMenu){
+            catalogMenu.classList.toggle('_submenu-show');
+            targetElement.classList.toggle('_submenu-active');
+            subMenu.classList.toggle('_submenu-open');
+        }else{
+            console.log('Поломка');
+        }
+        e.preventDefault();
+    }
+}
